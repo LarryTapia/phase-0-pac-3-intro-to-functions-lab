c1d69b437bd731b68e53caf73b3143430d52a694
Larry Tapia <tapialarry1@gmail.com>
2023-06-29 02:45:16 -0400

    completed lab

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ function whisper(string){
 }
 
 function logShout(string){
-    const spy = string;
-    console.log(spy.toUpperCase());
+    console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-    const spy = string;
-    console.log(spy.toLowerCase());
+    console.log(string.toLowerCase());
 }
 
 function sayHiToHeadphonedRoommate(string){
